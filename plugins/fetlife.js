5e7468a6c1224cd6474f67d2b868f34c230d94b6
Paul Spoerry <github@paulspoerry.com>
2016-04-20 15:58:03 -0500

    Adding updated FL codebase

--- plugins/fetlife.js
+++ plugins/fetlife.js
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 Romain Vallet <hoverzoom@gmail.com>
+﻿// Copyright (c) 2014 Romain Vallet <romain.vallet@gmail.com>
 // Licensed under the MIT license, read license.txt
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
@@ -7,7 +7,10 @@ hoverZoomPlugins.push( {
     name: 'fetlife',
     prepareImgLinks: function(callback) {
         var res = [];
-        hoverZoom.urlReplace(res, 'a img[src*="flpics"]', /_\d+\.jpg/, '_958.jpg');
+        hoverZoom.urlReplaceAndRemoveTokens(res, 'a img[src*=".fetlife"]', /_\d+\.jpg/, '_958.jpg');
+        // The following line grabs the image from its frame on an individual picture's page;
+        // unused since you can already access the full image from the gallery view
+        //hoverZoom.urlReplace(res, 'a.main_pic span.fake_img', /_\d+\.jpg/, '_958.jpg');
         callback($(res));
     }
 });
